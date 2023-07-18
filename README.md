fa978519b200221ee5c1bbc79dd85853772e62ff
braceal <braceal@umich.edu>
2023-07-18 10:01:20 -0500

    simulation logic

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # mdensemble
+
+Run molecular dynamics ensemble simulations in parallel using OpenMM.
+
+## Table of Contents
+- [mdensemble](#mdensemble)
+  - [Table of Contents](#table-of-contents)
+  - [Installation](#installation)
+
+## Installation
+
+To install `mdensemble`:
+```bash
+git clone https://github.com/braceal/mdensemble
+cd mdensemble
+python -m venv env
+source env/bin/activate
+make install
+```
---
+++ mdensemble/simulate.py
@@ -0,0 +1,312 @@
+import random
+import shutil
+from pathlib import Path
+from typing import Dict, Optional, Tuple, Union
+
+from pydantic import BaseSettings
+
+try:
+    import openmm
+    import openmm.app as app
+    import openmm.unit as u
+except ImportError:
+    pass  # For testing purposes
+
+PathLike = Union[str, Path]
+
+
+def _configure_amber_implicit(
+    pdb_file: PathLike,
+    top_file: Optional[PathLike],
+    dt_ps: float,
+    temperature_kelvin: float,
+    heat_bath_friction_coef: float,
+    platform: "openmm.Platform",
+    platform_properties: Dict[str, str],
+) -> Tuple["app.Simulation", Optional["app.PDBFile"]]:
+    """Helper function to configure implicit amber simulations with openmm."""
+    # Configure system
+    if top_file is not None:
+        pdb = None
+        top = app.AmberPrmtopFile(str(top_file))
+        system = top.createSystem(
+            nonbondedMethod=app.CutoffNonPeriodic,
+            nonbondedCutoff=1.0 * u.nanometer,
+            constraints=app.HBonds,
+            implicitSolvent=app.OBC1,
+        )
+    else:
+        pdb = app.PDBFile(str(pdb_file))
+        top = pdb.topology
+        forcefield = app.ForceField("amber14-all.xml", "implicit/gbn2.xml")
+        system = forcefield.createSystem(
+            top,
+            nonbondedMethod=app.CutoffNonPeriodic,
+            nonbondedCutoff=1.0 * u.nanometer,
+            constraints=app.HBonds,
+        )
+
+    # Configure integrator
+    integrator = openmm.LangevinIntegrator(
+        temperature_kelvin * u.kelvin,
+        heat_bath_friction_coef / u.picosecond,
+        dt_ps * u.picosecond,
+    )
+    integrator.setConstraintTolerance(0.00001)
+
+    sim = app.Simulation(top, system, integrator, platform, platform_properties)
+
+    # Returning the pdb file object for later use to reduce I/O.
+    # If a topology file is passed, the pdb variable is None.
+    return sim, pdb
+
+
+def _configure_amber_explicit(
+    top_file: PathLike,
+    dt_ps: float,
+    temperature_kelvin: float,
+    heat_bath_friction_coef: float,
+    platform: "openmm.Platform",
+    platform_properties: Dict[str, str],
+    explicit_barostat: str,
+) -> "app.Simulation":
+    """Helper function to configure explicit amber simulations with openmm."""
+    top = app.AmberPrmtopFile(str(top_file))
+    system = top.createSystem(
+        nonbondedMethod=app.PME,
+        nonbondedCutoff=1.0 * u.nanometer,
+        constraints=app.HBonds,
+    )
+
+    # Congfigure integrator
+    integrator = openmm.LangevinIntegrator(
+        temperature_kelvin * u.kelvin,
+        heat_bath_friction_coef / u.picosecond,
+        dt_ps * u.picosecond,
+    )
+
+    if explicit_barostat == "MonteCarloBarostat":
+        system.addForce(
+            openmm.MonteCarloBarostat(1 * u.bar, temperature_kelvin * u.kelvin)
+        )
+    elif explicit_barostat == "MonteCarloAnisotropicBarostat":
+        system.addForce(
+            openmm.MonteCarloAnisotropicBarostat(
+                (1, 1, 1) * u.bar, temperature_kelvin * u.kelvin, False, False, True
+            )
+        )
+    else:
+        raise ValueError(f"Invalid explicit_barostat option: {explicit_barostat}")
+
+    sim = app.Simulation(
+        top.topology, system, integrator, platform, platform_properties
+    )
+
+    return sim
+
+
+def configure_simulation(
+    pdb_file: PathLike,
+    top_file: Optional[PathLike],
+    solvent_type: str,
+    gpu_index: int,
+    dt_ps: float,
+    temperature_kelvin: float,
+    heat_bath_friction_coef: float,
+    checkpoint_file: Optional[PathLike] = None,
+    explicit_barostat: str = "MonteCarloBarostat",
+    run_minimization: bool = True,
+    set_positions: bool = True,
+    set_velocities: bool = False,
+) -> "app.Simulation":
+    """Configure an OpenMM amber simulation.
+    Parameters
+    ----------
+    pdb_file : PathLike
+        The PDB file to initialize the positions (and topology if
+        `top_file` is not present and the `solvent_type` is `implicit`).
+    top_file : Optional[PathLike]
+        The topology file to initialize the systems topology.
+    solvent_type : str
+        Solvent type can be either `implicit` or `explicit`, if `explicit`
+        then `top_file` must be present.
+    gpu_index : int
+        The GPU index to use for the simulation.
+    dt_ps : float
+        The timestep to use for the simulation.
+    temperature_kelvin : float
+        The temperature to use for the simulation.
+    heat_bath_friction_coef : float
+        The heat bath friction coefficient to use for the simulation.
+    checkpoint_file : Optional[PathLike], optional
+        The checkpoint file to load the simulation from, by default None.
+    explicit_barostat : str, optional
+        The barostat used for an `explicit` solvent simulation can be either
+        "MonteCarloBarostat" by deafult, or "MonteCarloAnisotropicBarostat".
+    run_minimization : bool, optional
+        Whether or not to run energy minimization, by default True.
+    set_positions : bool, optional
+        Whether or not to set positions (Loads the PDB file), by default True.
+    set_velocities : bool, optional
+        Whether or not to set velocities to temperature, by default True.
+    Returns
+    -------
+    app.Simulation
+        Configured OpenMM Simulation object.
+    """
+    # Configure hardware
+    try:
+        platform = openmm.Platform.getPlatformByName("CUDA")
+        platform_properties = {
+            "DeviceIndex": str(gpu_index),
+            "CudaPrecision": "mixed",
+        }
+    except Exception:
+        try:
+            platform = openmm.Platform.getPlatformByName("OpenCL")
+            platform_properties = {"DeviceIndex": str(gpu_index)}
+        except Exception:
+            platform = openmm.Platform.getPlatformByName("CPU")
+            platform_properties = {}
+
+    # Select implicit or explicit solvent configuration
+    if solvent_type == "implicit":
+        sim, pdb = _configure_amber_implicit(
+            pdb_file,
+            top_file,
+            dt_ps,
+            temperature_kelvin,
+            heat_bath_friction_coef,
+            platform,
+            platform_properties,
+        )
+    else:
+        assert solvent_type == "explicit"
+        assert top_file is not None
+        pdb = None
+        sim = _configure_amber_explicit(
+            top_file,
+            dt_ps,
+            temperature_kelvin,
+            heat_bath_friction_coef,
+            platform,
+            platform_properties,
+            explicit_barostat,
+        )
+
+    # Load checkpoint file
+    if checkpoint_file is not None:
+        sim.loadCheckpoint(checkpoint_file)
+        return sim
+
+    # Set the positions
+    if set_positions:
+        if pdb is None:
+            pdb = app.PDBFile(str(pdb_file))
+        sim.context.setPositions(pdb.getPositions())
+
+    # Set velocities to temperature
+    if set_velocities:
+        sim.context.setVelocitiesToTemperature(
+            temperature_kelvin * u.kelvin, random.randint(1, 10000)
+        )
+
+    # Minimize energy and equilibrate
+    if run_minimization:
+        sim.minimizeEnergy()
+
+    return sim
+
+
+def copy_to_workdir(p: Path, workdir: Path) -> Path:
+    """Copy a file or directory to the workdir."""
+    if p.is_file():
+        try:
+            return Path(shutil.copy(p, workdir))
+        except shutil.SameFileError:
+            return p
+    else:
+        return Path(shutil.copytree(p, workdir / p.name))
+
+
+class MDSimulationSettings(BaseSettings):
+    """Settings for an MD simulation."""
+
+    solvent_type: str = "implicit"
+    """Solvent type can be either `implicit` or `explicit`."""
+    simulation_length_ns: float = 10
+    """The length of the simulation in nanoseconds."""
+    report_interval_ps: float = 50
+    """The interval at which to report the simulation in picoseconds."""
+    dt_ps: float = 0.002
+    """The timestep to use for the simulation."""
+    temperature_kelvin: float = 310.0
+    """The temperature to use for the simulation."""
+    heat_bath_friction_coef: float = 1.0
+    """The heat bath friction coefficient to use for the simulation."""
+    explicit_barostat: str = "MonteCarloBarostat"
+    """The barostat used for an `explicit` solvent simulation can be either
+    "MonteCarloBarostat" by default, or "MonteCarloAnisotropicBarostat"."""
+
+
+def run_simulation(pdb_file: Path, workdir: Path, config: MDSimulationSettings) -> None:
+    # Select pdb_file input directory
+    input_dir = pdb_file.parent
+
+    # Copy PDB file to workdir
+    pdb_file = copy_to_workdir(pdb_file, workdir)
+
+    # Discover topology file and copy to workdir
+    top_file = next(input_dir.glob("*.top"), None)
+    if top_file is None:
+        top_file = next(input_dir.glob("*.prmtop"), None)
+    if top_file is not None:
+        top_file = copy_to_workdir(top_file, workdir)
+
+    # Discover checkpoint file and copy to workdir
+    checkpoint_file = next(input_dir.glob("*.chk"), None)
+    if checkpoint_file is not None:
+        checkpoint_file = copy_to_workdir(checkpoint_file, workdir)
+
+    # Initialize an OpenMM simulation
+    sim = configure_simulation(
+        pdb_file=pdb_file,
+        top_file=top_file,
+        solvent_type=config.solvent_type,
+        gpu_index=0,
+        dt_ps=config.dt_ps,
+        temperature_kelvin=config.temperature_kelvin,
+        heat_bath_friction_coef=config.heat_bath_friction_coef,
+        checkpoint_file=checkpoint_file,
+        explicit_barostat=config.explicit_barostat,
+    )
+
+    # openmm typed variables
+    dt_ps = config.dt_ps * u.picoseconds
+    report_interval_ps = config.report_interval_ps * u.picoseconds
+    simulation_length_ns = config.simulation_length_ns * u.nanoseconds
+
+    # Steps between reporting DCD frames and logs
+    report_steps = int(report_interval_ps / dt_ps)
+    # Number of steps to run each simulation
+    nsteps = int(simulation_length_ns / dt_ps)
+
+    # Set up reporters to write simulation trajectory file, logs, and checkpoints
+    traj_file = workdir / "sim.dcd"
+    sim.reporters.append(app.DCDReporter(traj_file, report_steps))
+    sim.reporters.append(
+        app.StateDataReporter(
+            str(workdir / "sim.log"),
+            report_steps,
+            step=True,
+            time=True,
+            speed=True,
+            potentialEnergy=True,
+            temperature=True,
+            totalEnergy=True,
+        )
+    )
+    sim.reporters.append(app.CheckpointReporter("checkpoint.chk", report_steps))
+
+    # Run simulation
+    sim.step(nsteps)

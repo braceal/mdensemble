ce69feb76d7519e79217371977858f7e5d00c2d5
braceal <braceal@umich.edu>
2023-07-21 10:53:43 -0500

    test simulate entry

--- tests/test_simulate.py
+++ tests/test_simulate.py
@@ -1,3 +1,4 @@
+from argparse import ArgumentParser
 from pathlib import Path
 
 from mdensemble.simulate import MDSimulationSettings, run_simulation
@@ -5,11 +6,13 @@ from mdensemble.simulate import MDSimulationSettings, run_simulation
 DATA_PATH = Path(__file__).parent.parent / "data"
 
 
-def test_run_simulation() -> None:
+def _test_run_simulation(
+    simulation_length_ns: float, report_interval_ps: float
+) -> None:
     config = MDSimulationSettings(
         solvent_type="explicit",
-        simulation_length_ns=0.02,
-        report_interval_ps=10,
+        simulation_length_ns=simulation_length_ns,
+        report_interval_ps=report_interval_ps,
         explicit_barostat="MonteCarloAnisotropicBarostat",
     )
 
@@ -19,5 +22,15 @@ def test_run_simulation() -> None:
 
     run_simulation(input_dir, output_dir, config)
 
+
+def test_run_simulation() -> None:
+    _test_run_simulation(simulation_length_ns=0.02, report_interval_ps=10)
+
+
 if __name__ == "__main__":
-    test_run_simulation()
+    # Entry point to test different configurations to check the ns/day
+    parser = ArgumentParser()
+    parser.add_argument("-n", "--simulation_length_ns", type=float, default=0.02)
+    parser.add_argument("-r", "--report_interval_ps", type=float, default=10)
+    args = parser.parse_args()
+    _test_run_simulation(args.simulation_length_ns, args.report_interval_ps)
